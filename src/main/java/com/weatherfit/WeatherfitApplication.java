bb1056e54711e7809394dd39f0d036584f743dd0
gilhanbit <rlaqnwksp@gmail.com>
2025-06-23 04:03:45 +0900

    add

--- src/main/java/com/weatherfit/WeatherfitApplication.java
+++ src/main/java/com/weatherfit/WeatherfitApplication.java
@@ -1,12 +1,16 @@
 package com.weatherfit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class WeatherfitApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WeatherfitApplication.class, args);
 	}
 
