a6e914b310b02367dae5b056ee52a35775864abc
gilhanbit <rlaqnwksp@gmail.com>
2025-06-23 03:39:41 +0900

    project

---
+++ src/main/java/com/weatherfit/WeatherfitApplication.java
@@ -0,0 +1,13 @@
+package com.weatherfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherfitApplication.class, args);
+	}
+
+}
