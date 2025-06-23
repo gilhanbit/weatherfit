e38cf9e26b6cee37f2a57c5b0d295290564ee08e
gilhanbit <rlaqnwksp@gmail.com>
2025-06-23 12:30:32 +0900

    test

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,9 +1,20 @@
 package com.test;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Test {
 
+    @ResponseBody
+    @GetMapping("/test1")
+    public String test1(){
+        return "<h2>test</h2>";
+    }
 
+    @GetMapping("/test2")
+    public String test(){
+        return "test/test";
+    }
 }
