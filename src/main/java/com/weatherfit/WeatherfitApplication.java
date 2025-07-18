f2ac1e397ac61776b6461879177041f5c74083cc
gilhanbit <rlaqnwksp@gmail.com>
2025-07-18 19:50:33 +0900

    modify

--- src/main/java/com/weatherfit/WeatherfitApplication.java
+++ src/main/java/com/weatherfit/WeatherfitApplication.java
@@ -2,11 +2,12 @@ package com.weatherfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class WeatherfitApplication {
+public class WeatherfitApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherfitApplication.class, args);
